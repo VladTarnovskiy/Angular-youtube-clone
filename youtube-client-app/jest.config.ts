17408c860af99d31aa1d60ed3e20c1094a90b101
vladtarnovskiy <vladtarnovski989@gmail.com>
2023-12-22 14:13:30 +0300

    fix: jest absolute path resolve error

--- youtube-client-app/jest.config.ts
+++ youtube-client-app/jest.config.ts
@@ -15,6 +15,9 @@ const config: Config = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
   testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1',
+  },
 };
 
 export default config;
